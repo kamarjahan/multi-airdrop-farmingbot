6200b1b32129f9ed65abace38b1eee48b602ed5e
KAMARJAHAN <ztenkammu@gmail.com>
2024-08-20 20:28:44 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -6,8 +6,9 @@ from pyrogram import Client, filters
 async def help(client, message):
     help_text = (
         "Here is how you can use this bot:\n"
-        "- `/start`: Start the bot and see the main menu.\n"
-        "- `/refer`: Get your referral link to share with others.\n"
-        "- `/help`: Show this help message."
+        "- /start: Start the bot and see the main menu.\n"
+        "- /refer: Get your referral link to share with others.\n"
+        "- /help: Show this help message."
+        "- /report: to help admins to add new projects and any other bugs and add features."
     )
     await message.reply_text(help_text)
