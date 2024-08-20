d8b9cdc0580e631bff418a3b31676e20b9c6b114
KAMARJAHAN <ztenkammu@gmail.com>
2024-08-20 20:35:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
-# bot.py
+from pyrogram import Client, idle
 
-from pyrogram import Client
-import config
+# Import your plugins
+from plugins import start, refer, report
 
-app = Client(
-    "crypto_airdrop_bot",
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN,
-    plugins=dict(root="plugins")
-)
+# Configuration
+API_ID = '17875613'
+API_HASH = '6798f54a7f74e94f2ef0923fba8a8377'
+BOT_TOKEN = '7513524486:AAFlIPgdbEsui9DNmTqAXTzQr1t37O_E6vc'
+
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 if __name__ == "__main__":
-    app.run()
+    app.start()
+    print("Bot is running!")
+    idle()  # Keep the bot running
+    app.stop()
